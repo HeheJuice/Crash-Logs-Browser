36099fbe9657e36838c246bab56be8de95bf49a7
HeheJuice <minecraftplayer11964@gmail.com>
2026-08-10 22:41:48 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
+implementation ("moe.shizuku.privileged:api:13.1.5")
+    implementation ("moe.shizuku.privileged:provider:13.1.5")
 }
+
+
